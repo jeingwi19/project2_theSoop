0d6490abc806f1e69e43e38891ca4c69ddcee870
Jeongeeun <53941317+jeingwi19@users.noreply.github.com>
2020-11-09 17:02:47 +0900

    add and fix it
    201109/html/css

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    var scrollT;
+    var timer = 0;
+
+    var $gnb = $('#gnb');
+
+    //문서가 로드되면 #header.load를 추가해서 브라우저 상단에서 아래로 내려오게
+
+    $('#header').addClass('load');
+    $('#content .top_visual').addClass('on');
+    
+})
