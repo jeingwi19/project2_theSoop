80e65db78a935e30c05f71f6c56950b398020a93
Jeongeeun <53941317+jeingwi19@users.noreply.github.com>
2020-11-16 17:30:49 +0900

    change html, css, js
    201116_modify

--- js/main.js
+++ js/main.js
@@ -67,16 +67,16 @@ $(document).ready(function () {
         spaceBetween: 10, //각 슬라이더 사이 공간
         slidesPerView: 'auto', //한 화면에 보여질 슬라이더 개수 기본1
         centeredSlides: true, //슬라이더 가운데 중요 슬라이더 위치
-        scrollbar: {
-            el: '.swiper-scrollbar',
-            draggable: true,    //스크롤바를 직접 드래그해서 이동가능
-        },
         a11y: {
             //prevSlideMessage: '이전 슬라이드',
             //nextSlideMessage: '다음 슬라이드',
             firstSlideMessage: '첫번째 슬라이드',
             lastSlideMessage: '마지막 슬라이드',
-        }
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
     });
 
 
